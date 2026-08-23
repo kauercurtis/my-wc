87ed09415f3b5b86693d5ea6c880efbc384d08e2
Curtis Kauer <kauercurtis@gmail.com>
2026-08-23 11:45:27 -0700

    implemented -m for character count

--- commands.c
+++ commands.c
@@ -3,6 +3,7 @@
 int fd;
 char temp[101];
 int temp_index = 0;
+int temp_len = 0;
 
 int
 open_file (char file_name[])
@@ -106,4 +107,68 @@ word_count ()
 		words += word_processing(buf, nb_read);
 	}
 	return words;
+}
+
+int
+char_processing (char buffer[], int bytes_read)
+{
+	int char_count = 0;
+	for (int i = 0; i < bytes_read; i++)
+	{
+		if (temp_len > 1 && temp_index == temp_len - 1)
+		{
+			temp[0] = '\0';
+			temp_index = 0;
+			temp_len = 0;
+			char_count++;
+		}
+		else if ((buffer[i] & 0x80) == 0)
+		{
+			char_count++;
+		}
+		else if ((buffer[i] & 0xE0) == 0xC0)
+		{
+			temp_len = 2;
+			temp[temp_index] = buffer[i];
+			temp_index++;
+		}
+		else if ((buffer[i] & 0xf0) == 0xE0)
+		{
+			temp_len = 3;
+			temp[temp_index] = buffer[i];
+			temp_index++;
+		}
+		else if ((buffer[i] & 0xF8) == 0xF0)
+		{
+			temp_len = 4;
+			temp[temp_index] = buffer[i];
+			temp_index++;
+		}
+		else
+		{
+			temp[temp_index] = buffer[i];
+			temp_index++;
+		}
+	}
+	return char_count;
+}
+
+int
+character_count ()
+{
+	int characters = 0;
+	char buf[100 + 1];
+	int nb_read = -1;
+	while (nb_read != 0)
+	{
+		nb_read = read(fd, buf, 100);
+		if (nb_read == -1)
+		{
+			fprintf(stderr, "Error: Reading error, got -1.");
+			break;
+		}
+		buf[nb_read] = '\0';
+		characters += char_processing(buf, nb_read);
+	}
+	return characters;
 }
--- commands.h
+++ commands.h
@@ -10,6 +10,7 @@
 extern int fd;
 extern char temp[101];
 extern int temp_index;
+extern int temp_len;
 
 int
 open_file (char file_name[]);
@@ -28,5 +29,11 @@ word_count ();
 
 int
 word_processing (char buffer[], int bytes_read);
+
+int
+character_count ();
+
+int
+char_processing (char buffer[], int bytes_read);
 
 #endif /* COMMANDS_H */
